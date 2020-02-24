599d982f0e27f847244627a4a2463fbc54a2b18f
Gina Sekhar <ginasekhar@yahoo.com>
2020-02-24 00:18:59 -0500

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
 class Filters extends React.Component {
+  handleClick = (event) => {
+    // event.preventDefault()
+    this.props.onFindPetsClick();
+  }
+
+  handleTypeChange = (event) => {
+    // event.preventDefault()
+    this.props.onChangeType(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select onChange={this.handleTypeChange} name="type" id="type">
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +25,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.handleClick}>Find pets</button>
         </div>
       </div>
     )
